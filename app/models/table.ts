5ed40053df363ffdf51c882afee70c3af42f8ff7
Paul Lee <plee0125@gmail.com>
2024-05-31 22:48:30 -0700

    updated table model and added game model

--- app/models/table.ts
+++ app/models/table.ts
@@ -1,6 +1,9 @@
+import { Card } from "./card"
+
 export class Table {
     private button: number;
     private seats: Array<boolean>;
+    private runout: Array<Card>
 
     constructor(button: number, seats: Array<boolean>) {
         this.button = button;
