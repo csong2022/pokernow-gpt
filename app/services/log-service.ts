85fd538fdaf0967f1784db0b42645529e3cc0dfc
Paul Lee <plee0125@gmail.com>
2024-06-05 19:50:58 -0700

    added testing for parsing json

--- app/services/log-service.ts
+++ app/services/log-service.ts
@@ -1,21 +1,6 @@
 const fetch = require('node-fetch')
 
-export const SUCCESS_RESPONSE = "success";
-export const ERROR_RESPONSE = "error";
-
-export type SuccessResponse<D> = {
-    code: typeof SUCCESS_RESPONSE;
-    data: D;
-}
-
-export type ErrorResponse<E = Error> = {
-    code: typeof ERROR_RESPONSE;
-    error: E;
-}
-
-export type BaseFetchResponse<D, E> = Promise<SuccessResponse<D> | ErrorResponse<E>>;
-
-//const endpoint = "/log?before_at=&after_at=171763427711700&mm=false&v=2"
+import type { Response } from '../utils/error-handling-utils.ts';
 
 export const fetchData = async <D, E=Error>(
     method: string = "GET",
@@ -23,7 +8,7 @@ export const fetchData = async <D, E=Error>(
     before: string = "",
     after: string = "",
     bodyData?: Record<string, unknown>
-): BaseFetchResponse<D, E> => {
+): Response<D, E> => {
     let fetchOptions: RequestInit = {
         headers: {
             'Content=type': 'applications/json'
