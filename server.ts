9de29808f231d4133757f3e57c1130a7e95773c4
Chen Song <csong2022@Chens-MacBook-Pro.local>
2024-06-05 18:18:54 -0700

    update error handling, starting work on app cycle

--- server.ts
+++ server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import player_router from './app/routes/player-routes.ts';
+import prompt from 'prompt-sync';
+import * as puppeteer_service from './app/webdriver/puppeteer-service.ts';
 
 const app = express();
 const port = 8080;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (req: any, res:any) => {
     res.json({'message': 'ok'});
 })
 
@@ -16,3 +18,19 @@ app.listen(
     port,
     () => console.log('App listening on http://localhost:${PORT}')
 )
+
+const io = prompt();
+const game_id = io("Enter the PokerNow game id (ex. https://www.pokernow.club/games/{game_id}): ")
+console.log(`The PokerNow game with id: ${game_id} will now open.`);
+
+await puppeteer_service.init(game_id);
+
+const name = io("What is your desired player name? ");
+console.log(`Your player name will be ${name}.` )
+
+const stack_size = io("What is your desired stack size? ");
+console.log(`Your initial stack size will be ${stack_size}.`)
+
+console.log(`Attempting to enter table with name: ${name} and stack size: ${stack_size}.`);
+await puppeteer_service.enterTable(name, Number(stack_size));
+
