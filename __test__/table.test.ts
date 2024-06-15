3af9888263574d6183810a876c4554819d8ae436
Paul Lee <plee0125@gmail.com>
2024-06-14 19:47:03 -0700

    fixed the tests

--- __test__/table.test.ts
+++ __test__/table.test.ts
@@ -4,39 +4,8 @@ import { PlayerData } from "../app/models/player-data.ts"
 //const table = require('./table')
 
 test('properly set variables', () => {
-    let test1 = new Table(3, "NLH", 1)
-    expect(test1.getStakes()).toBe(1)
-    expect(test1.getGameType()).toBe("NLH")
-    expect(test1.getButton()).toBe(3)
-})
-
-test('update button tests', () => {
-    let test1 = new Table(3, "NLH", 1)
-    expect(test1.getNextButton(3)).toBe(4)
-    expect(test1.getNextButton(10)).toBe(0)
-    test1.updateButton(6)
-    expect(test1.getButton()).toBe(6)
-})
-
-test('update player tests', () => {
-    let test1 = new Table(3, "NLH", 1)
-    let player1data = new PlayerData('alwayswins')
-    let player1 = new Player("voldlord", 100, 1, player1data)
-    test1.addPlayer(1, player1)
-    expect(test1.getNumPlayers()).toBe(1)
-    test1.removePlayer(1)
-    expect(test1.getNumPlayers()).toBe(0)
-})
-
-test('betting tests', () => {
-    let test1 = new Table(3, "NLH", 1)
-    let player1data = new PlayerData('alwayswins')
-    let player1 = new Player("voldlord", 100, 1, player1data)
-    test1.addPlayer(1, player1)
-    expect(test1.getNumPlayers()).toBe(1)
-    test1.updatePot(100)
-    expect(test1.getPot()).toBe(100)
-    //expect(test1.getNumPotPlayers()).toBe(1)
-    //test1.foldPlayer(1)
-    //expect(test1.getNumPotPlayers()).toBe(0)
+    let table1 = new Table()
+    expect(table1.getPot()).toBe(0)
+    table1.updatePot(1)
+    expect(table1.getPot()).toBe(1)
 })
