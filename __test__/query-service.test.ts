bafc66866117cc6f9435eb9d3d7b56241400c830
snmetcalf <smetcalf@berkeley.edu>
2024-06-14 20:53:04 -0700

    adding position to query

--- __test__/query-service.test.ts
+++ __test__/query-service.test.ts
@@ -6,21 +6,29 @@ const testArray = [
     ['hjf1Arb46X', 'jep', 'posts', 'posts a small blind of 10', '10']
 ];
 
-function generatePrompt(logs: Array<Array<string>>): string{ 
+const idDict = {
+    'bmrA62QMH-': 'SB', 
+    '3so3OqbMfo': 'BB', 
+    'hjf1Arb46X': 'BU'
+}
+
+function generatePrompt(logs: Array<Array<string>>, idDictionary: Record<string, string>): string { 
     // Get the logs in a designed format
-    const formattedLogs = logs.map(log => `ID ${log[0]} User ${log[1]} ${log[2]} ${log[3]}.`);
+    const formattedLogs = logs.map(log => {
+        const position = idDictionary[log[0]] ? ` in the ${idDictionary[log[0]]}` : '';
+        return `ID ${log[0]} User ${log[1]}${position} ${log[2]} ${log[3]}.`;
+    });
     const combinedLogs = formattedLogs.join(' ');
 
     // After last log propose a question
     const lastLog = logs[logs.length - 1];
-    const question = `What should ID ${lastLog[0]} User ${lastLog[1]} do?`;
+    const lastPosition = idDictionary[lastLog[0]] ? ` in the ${idDictionary[lastLog[0]]}` : '';
+    const question = `What should ID ${lastLog[0]} User ${lastLog[1]}${lastPosition} do?`;
 
-    return `${combinedLogs} ${question}`
+    return `${combinedLogs} ${question}`;
 }
 
-test('test chens table 123', async () => {
-    const prompt = generatePrompt(testArray);
+test('test query format', async () => {
+    const prompt = generatePrompt(testArray, idDict);
     console.log(prompt)
 })
-/* const prompt = generatePrompt(testArray);
-console.log(prompt) */
