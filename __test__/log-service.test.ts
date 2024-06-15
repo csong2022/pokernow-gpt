c8563b89d0cb990266f0684af30e665e3f8f75c3
Paul Lee <plee0125@gmail.com>
2024-06-14 18:30:40 -0700

    added pruning until first starting log

--- __test__/log-service.test.ts
+++ __test__/log-service.test.ts
@@ -1,14 +1,14 @@
 import { fetchData, getCreatedAt } from "../app/services/log-service.ts"
 import { SUCCESS_RESPONSE, ERROR_RESPONSE} from '../app/utils/error-handling-utils.ts';
 import { getData, getMsg, getLast, getFirst } from '../app/services/log-service.ts';
-import { getPlayer, getPlayerAction, getFirstWord, validateAllMsg, validateMsg } from "../app/services/message-service.ts";
+import { getPlayer, getPlayerAction, getFirstWord, validateAllMsg, validateMsg, pruneStarting } from "../app/services/message-service.ts";
 import { Table } from "../app/models/table.ts";
 
 test('test chens table', async () => {
     const log = await fetchData("GET", "pgltNd4w17e6J4JXouHm6dw5l")
     if (log.code === SUCCESS_RESPONSE) {
-        //console.log('success', log.data)
-        let res = getData(log)
+        console.log('success', log.data)
+        /* let res = getData(log)
         let res1 = getMsg(res)
         console.log(res1)
         let p1 = getPlayer(res1[0])
@@ -18,7 +18,7 @@ test('test chens table', async () => {
         let p3 = getFirstWord(p2)
         console.log(p3)
         let res2 = getCreatedAt(res)
-        console.log(res2)
+        console.log(res2) */
         //console.log(res.logs[0].msg)
     }
     if (log.code === ERROR_RESPONSE) {
@@ -32,9 +32,13 @@ test('test chens table 123', async () => {
         //console.log('success', log.data)
         let res = getData(log)
         let res1 = getMsg(res)
-        console.log(res1)
+        console.log("messages", res1)
+        let prune = pruneStarting(res1)
+        console.log("pruned until starting", prune)
         let res5 = validateAllMsg(res1)
-        console.log(res5)
+        console.log("all msgs", res5)
+        let pruneres = validateAllMsg(prune)
+        console.log("valid actions until starting", pruneres)
         let t = new Table(1, "NLH", 1)
         t.nextRound()
         t.processLogs(res5)
