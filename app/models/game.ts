a1c99bb210b055189341a326913d61aec6fb100e
Chen Song <csong2022@mac.mynetworksettings.com>
2024-06-14 22:57:49 -0400

    refactoring, paul fix table.ts

--- app/models/game.ts
+++ app/models/game.ts
@@ -1,10 +1,15 @@
+import { Dictionary } from "../utils/data-structures.ts";
+import { Player } from "./player.ts"
+
 export class Game {
     private game_id: string;
     private stakes: number;
+    private players: Map<string, Player>;
 
     constructor(game_id: string, stakes: number) {
         this.game_id = game_id;
         this.stakes = stakes;
+        this.players = new Map<string, Player>();
     }
 
     public getGameId() {
