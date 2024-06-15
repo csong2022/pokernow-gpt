c3d249053604081963aa8bac328d21946086d39b
Chen Song <csong2022@mac.mynetworksettings.com>
2024-06-14 23:46:02 -0400

    implemented getGameInfo, updated error handling responses

--- app/utils/error-handling-utils.ts
+++ app/utils/error-handling-utils.ts
@@ -4,6 +4,7 @@ export const ERROR_RESPONSE = "error";
 export type SuccessResponse<D> = {
     code: typeof SUCCESS_RESPONSE;
     data: D;
+    msg: string;
 }
 
 export type ErrorResponse<E = Error> = {
@@ -15,7 +16,7 @@ export type Response<D, E> = Promise<SuccessResponse<D> | ErrorResponse<E>>;
 
 export function log_response<D, E=Error>(res: SuccessResponse<D> | ErrorResponse<E>): string {
     if (res.code == SUCCESS_RESPONSE) {
-        console.log(res.data);
+        console.log(res.msg);
     } else {
         console.log(res.error);
     }
