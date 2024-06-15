fb50c0ba4539dfb2e2253580d492441dcc079329
snmetcalf <smetcalf@berkeley.edu>
2024-06-14 20:27:30 -0700

    testing for query

--- app/services/query-service.ts
+++ app/services/query-service.ts
@@ -1,2 +1,9 @@
 // Creating query service
+import { fetchData, getCreatedAt, getData, getMsg, getLast, getFirst } from '../app/services/log-service.ts';
 
+// const response = await fetchData()
+// if response.code === "success") {}
+
+
+let prompt: string = 'testing';
+console.log(prompt);
