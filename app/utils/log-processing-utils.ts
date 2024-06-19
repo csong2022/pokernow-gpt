27f7fb0948e7eb57c3ed9e9bd00dbcbcdc9f4de8
Paul Lee <plee0125@gmail.com>
2024-06-18 22:48:53 -0700

    added enums and refactored

---
+++ app/utils/log-processing-utils.ts
@@ -0,0 +1,16 @@
+export enum Actions {
+    BET = "bets",
+    CALL = "calls",
+    FOLD = "folds",
+    RAISE = "raises",
+    POST = "posts",
+    CHECK = "checks",
+}
+
+export enum Streets {
+    PREFLOP = "Preflop",
+    FLOP = "Flop:",
+    TURN = "Turn:",
+    RIVER = "River:",
+}
+
