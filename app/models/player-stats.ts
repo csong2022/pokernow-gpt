44def5f6159777e1604fcab089573a5783109b48
Chen Song <csong2018@gmail.com>
2024-06-20 01:27:41 -0700

    update player stats implementation to use generated value in db

--- app/models/player-stats.ts
+++ app/models/player-stats.ts
@@ -3,9 +3,7 @@ export class PlayerStats {
     private total_hands: number;
     private walks: number;
     private vpip_hands: number;
-    private vpip_stat: number;
     private pfr_hands: number;
-    private pfr_stat: number;
 
     constructor(id: string, player_JSON?: any) {
         this.id = id;
@@ -13,16 +11,12 @@ export class PlayerStats {
             this.total_hands = player_JSON.total_hands;
             this.walks = player_JSON.walks;
             this.vpip_hands = player_JSON.vpip_hands;
-            this.vpip_stat = player_JSON.vpip_stat;
             this.pfr_hands = player_JSON.pfr_hands;
-            this.pfr_stat = player_JSON.pfr_stat;
         } else {
-            this.total_hands = 0
-            this.walks = 0
-            this.vpip_hands = 0
-            this.vpip_stat = 0
-            this.pfr_hands = 0
-            this.pfr_stat = 0
+            this.total_hands = 0;
+            this.walks = 0;
+            this.vpip_hands = 0;
+            this.pfr_hands = 0;
         }
     }
 
@@ -50,9 +44,8 @@ export class PlayerStats {
         this.vpip_hands = vpip
     }
 
-    public setVPIPStat(): void {
-        var vpip:number = this.vpip_hands / (this.getTotalHands() - this.getWalk());
-        this.vpip_stat = vpip
+    public computeVPIPStat(): number {
+        return this.vpip_hands / (this.total_hands - this.walks);
     }
 
     public getPFRHands(): number {
@@ -63,9 +56,8 @@ export class PlayerStats {
         this.pfr_hands = pfr
     }
 
-    public setPFRStat(): void {
-        var pfr:number = this.pfr_hands / (this.getTotalHands() - this.getWalk());
-        this.pfr_stat = pfr
+    public computePFRStat(): number {
+        return this.pfr_hands / (this.total_hands - this.walks);
     }
 
     public toJSON(): any {
@@ -74,9 +66,7 @@ export class PlayerStats {
             "total_hands": this.total_hands,
             "walks": this.walks,
             "vpip_hands": this.vpip_hands,
-            "vpip_stat": this.vpip_stat,
             "pfr_hands": this.pfr_hands,
-            "pfr_stat": this.pfr_stat
         }
     }
 }
