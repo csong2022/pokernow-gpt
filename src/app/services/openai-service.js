f63a204185f1fb1c499ba7ea168017f792b4adb7
Paul Lee <plee0125@gmail.com>
2024-06-26 00:39:04 -0700

    added queryGPT in openai service to tests

--- src/app/services/openai-service.js
+++ src/app/services/openai-service.js
@@ -2,15 +2,12 @@ import OpenAI from "openai";
 
 const openai = new OpenAI();
 
-export async function main(query) {
+export async function queryGPT(query) {
   const completion = await openai.chat.completions.create({
     messages: [{ role: "system", content: query }],
     model: "gpt-3.5-turbo",
   });
 
-  console.log(completion.choices[0]);
-  return completion.choices;
+  //console.log("response 0", completion.choices[0]);
+  return completion.choices[0];
 }
-
-msg = "You are a helpful assistant.";
-main(msg);
