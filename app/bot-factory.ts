599d900b0c596be32bc38a705b5bcc707d8d6cb3
Chen Song <csong2018@gmail.com>
2024-06-30 15:57:31 -0700

    bugfixing after refactoring

--- app/bot-factory.ts
+++ app/bot-factory.ts
@@ -16,7 +16,6 @@ async function init(puppeteer_service: PuppeteerService): Promise<Game> {
     console.log(`The PokerNow game with id: ${game_id} will now open.`);
     
     logResponse(await puppeteer_service.navigateToGame(game_id), bot_config.debug_mode);
-
     logResponse(await puppeteer_service.waitForGameInfo(), bot_config.debug_mode);
 
     console.log("Getting game info.");
@@ -34,6 +33,7 @@ const bot_factory = async function() {
     const puppeteer_service = new PuppeteerService(1000);
     await puppeteer_service.init();
     const log_service = new LogService(game_id);
+    await log_service.init();
     const game = await init(puppeteer_service);
     const bot = new Bot(log_service, puppeteer_service, game, bot_config.debug_mode, bot_config.query_retries);
     await bot.run();
