8655cdcda46b55e40770ee55902d2eb8aea9f8d2
Chen Song <csong2018@gmail.com>
2024-07-02 17:47:47 -0700

    fix defineRank when 10 is on board

--- test/unit/query.spec.ts
+++ test/unit/query.spec.ts
@@ -11,6 +11,7 @@ import { PlayerService } from "../../app/services/player-service.ts";
 import { SUCCESS_RESPONSE, ERROR_RESPONSE} from '../../app/utils/error-handling-utils.ts';
 import { postProcessLogs, postProcessLogsAfterHand, preProcessLogs } from "../../app/utils/log-processing-utils.ts";
 import { validateAllMsg } from "../../app/utils/message-processing-utils.ts";
+import { defineRank } from "../../app/helpers/construct-query-helper.ts";
 
 describe('query service test', async () => {
     it("should properly get logs and filter through them", async() => {
